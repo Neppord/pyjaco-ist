8d4c0f87b9a8a471b33dba8d434ea5babe780654
Samuel Ytterbrink <neppord@gmail.com>
2012-06-21 19:26:19 +0200

    More cleanup!

--- output.py
+++ output.py
@@ -62,36 +62,5 @@ def output(node):
                 )
     elif type(node) in [str, long, int, float]:
         return repr(node)
-    if node.__class__ == Num:
-        return str(node.n)
-    elif node.__class__ == Name:
-        return node.id
     elif node.__class__ == Attribute:
         return '%s.%s' % (output(node.value), output(node.attr))
-    elif node.__class__ == BinOp:
-        return "%s %s %s" %(
-            output(node.left),
-            output(node.op),
-            output(node.right)
-            )
-    elif node.__class__ == BoolOp:
-        return (" %s " % output(node.op)).join(
-            output(value) for value in node.values
-        )
-    elif node.__class__ == Compare:
-        comparators = [output(comparator) for comparator in node.comparators]
-        return (" %s " % output(And())).join(
-            "(%s %s %s)" % tp for tp in zip(
-                [output(node.left)] + comparators,
-                (output(op) for op in node.ops),
-                comparators
-                )
-        )
-    elif node.__class__ == Arguments:
-        return  ", ".join(output(arg) for arg in node.args) 
-    elif node.__class__ == Lambda:
-        return "function (%s) {return %s;}" % (
-            output(node.args),
-            output(node.body)
-        )
-    return ops.get(node.__class__,'')
